07d69b635bf8113c96d4e79eb9f6ed3a55837fa6
AlbertBarbashahinyan2 <117849540+AlbertBarbashahinyan2@users.noreply.github.com>
2022-12-03 00:10:06 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const utils = require('./utils');
+
+console.log(utils.matrixProduct([[1,2],[3,4]], [[5,6],[7,8]]));
+console.log(utils.sumOfMatrices([[1,2],[3,4]], [[5,6],[7,8]]));
+console.log(utils.sumOfEachRow([[1,2,3],[4,5,6]]));
+console.log(utils.trim("   Dolor id incididunt nostrud aliqua irure commodo tempor voluptate.  "));
+console.log(utils.countWords("   Dolor id incididunt nost rud  aliqua irure commodo  tempor voluptate.  "));
